ebcf453c4115fdc004e4c7f438c95b27c17d86c5
jjjiyoung0130 <jiyoung63@nate.com>
2024-04-11 15:45:40 +0900

    알고리즘 3문제 풀이

---
+++ Brilly/Lv1/정수제곱근판별.js
@@ -0,0 +1,27 @@
+/* 
+문제설명
+임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+제한 사항
+n은 1이상, 50000000000000 이하인 양의 정수입니다.
+*/
+
+// 프로그래머스 제출
+function solution(n) {
+  const sqrt = Math.sqrt(n);
+  return sqrt % 1 === 0 ? Math.pow(sqrt + 1, 2) : -1;
+}
+
+// 풀이 -> 제출은 리팩터링
+const j_solution = (n) => {
+  let sqrt = Math.sqrt(n);
+  if (sqrt % 1 === 0) {
+    answer = Math.pow(sqrt + 1, 2);
+  } else {
+    answer = -1;
+  }
+  return answer;
+};
+
+j_solution(120);
