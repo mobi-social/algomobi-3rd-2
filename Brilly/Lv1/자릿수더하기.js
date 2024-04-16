11a24edb2ca66272888460c49ab2bb489e2021ca
jjjiyoung0130 <jiyoung63@nate.com>
2024-04-16 22:31:57 +0900

    [2024.04.16] 알고리즘 3문제 풀이

---
+++ Brilly/Lv1/자릿수더하기.js
@@ -0,0 +1,23 @@
+/* 
+문제 설명
+자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+제한 사항
+N의 범위 : 100,000,000 이하의 자연수
+*/
+
+const solution = (n) => {
+  let result = 0;
+
+  const stringN = String(n).split("");
+  for (let num of stringN) {
+    result += parseInt(num);
+  }
+  return result;
+};
+
+// 다른 사람 풀이 => reduce를 활용해보자.
+function solution(n) {
+  return (n + "").split("").reduce((acc, curr) => acc + parseInt(curr), 0);
+}
